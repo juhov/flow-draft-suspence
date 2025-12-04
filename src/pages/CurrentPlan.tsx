f30a47d30ba23eff6da67fb5ba3b9a8d20ae4928
Juho Valtonen <juho.valtonen@ouraring.com>
2025-12-04 14:13:44 +0200

    example of targeted loading state

--- src/pages/CurrentPlan.tsx
+++ src/pages/CurrentPlan.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
 import { AccountInfo } from '../components/AccountInfo';
+import { Loading } from '../components/Loading';
 
 export function CurrentPlan() {
   return (
     <div>
       <h1>Current Plan</h1>
       <p>Your current plan information will be displayed here.</p>
-      <AccountInfo />
+      <Suspense fallback={<Loading />}>
+        <AccountInfo />
+      </Suspense>
     </div>
   )
 }
